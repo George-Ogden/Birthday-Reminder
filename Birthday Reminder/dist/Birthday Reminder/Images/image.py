2be248a7d554a2d7bbea6e2d4461d3adf6515cb8
George_Ogden <G02287@outlook.com>
2020-07-13 09:26:12 +0100

    removed compiled files

--- Birthday Reminder/dist/Birthday Reminder/Images/image.py
+++
@@ -1,9 +0,0 @@
-from PIL import ImageTk, Image
-
-def get_images():
-    #set up images
-    Balloons = ImageTk.PhotoImage(Image.open("Images/Balloons.png"))
-    Present = ImageTk.PhotoImage(Image.open("Images/Present.png"))
-    Cake = ImageTk.PhotoImage(Image.open("Images/Cake.png"))
-    Flowers = ImageTk.PhotoImage(Image.open("Images/Flowers.png"))
-    return Balloons,Present,Cake,Flowers
